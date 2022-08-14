8bf257df6ea0bf0fed43dadd7b938c9505c39b0e
Meshack <mbuvi.meshack99@gmail.com>
2022-08-15 00:22:47 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+var a, b;
+a = 2000;
+b = 2000;
+function add(a, b){
+    return (a + b)
+}
+function subtract(a, b){
+    return (a - b)
+}
+function multiply(a, b){
+    return (a * b)
+}
+function divide(a, b){
+    return (a / b)
+}
+function increment(a, b){
+    return (a += 1)
+}
+function decrement(b){
+    return (b -= 1)
+}
+function makeInt(n){
+    return parseInt(n,10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
